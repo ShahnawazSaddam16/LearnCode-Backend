e4e507868a644396581f1b29f02936725323128a
ShahnawazSaddam16 <saddammaqsood4321@gmail.com>
2026-07-21 15:43:03 +0500

    crerating contrller and models for buy course

--- src/controllers/course.js
+++ src/controllers/course.js
@@ -1,4 +1,5 @@
 const Course = require("../models/course");
+const userCourses = require("../models/userCourses");
 const courseData = require("../utils/courseData");
 
 const seedCourses = async () => {
@@ -12,7 +13,7 @@ const getAllCourses = async (req, res) => {
   try {
     await seedCourses();
 
-    const courses = await Course.find({ isPublished: true }).select("-__v");
+    const courses = await Course.find({ isPublished: true }).select("-__v -githubRepo");
 
     res.status(200).json({
       success: true,
@@ -43,9 +44,29 @@ const getCourseBySlug = async (req, res) => {
       });
     }
 
+    let hasPurchased = false;
+
+    if (req.user) {
+      const purchase = await userCourses.findOne({
+        user: req.user._id,
+        course: course._id,
+      });
+
+      if (purchase) {
+        hasPurchased = true;
+      }
+    }
+
+    const courseObj = course.toObject();
+
+    if (!hasPurchased) {
+      delete courseObj.githubRepo;
+    }
+
     res.status(200).json({
       success: true,
-      course,
+      course: courseObj,
+      hasPurchased,
     });
   } catch (err) {
     console.log("Get Course Error:", err);
@@ -57,4 +78,63 @@ const getCourseBySlug = async (req, res) => {
   }
 };
 
-module.exports = { getAllCourses, getCourseBySlug };
+const buyCourse = async (req, res) => {
+  try {
+    const { slug } = req.body;
+
+    if (!slug) {
+      return res.status(400).json({
+        success: false,
+        message: "Course slug is required",
+      });
+    }
+
+    const course = await Course.findOne({ slug, isPublished: true });
+
+    if (!course) {
+      return res.status(404).json({
+        success: false,
+        message: "Course not found",
+      });
+    }
+
+    const alreadyPurchased = await userCourses.findOne({
+      user: req.user._id,
+      course: course._id,
+    });
+
+    if (alreadyPurchased) {
+      return res.status(400).json({
+        success: false,
+        message: "Course already purchased",
+      });
+    }
+
+    const purchase = await userCourses.create({
+      user: req.user._id,
+      email: req.user.email,
+      course: course._id,
+      title: course.title,
+      price: course.price,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Course purchased successfully",
+      purchase: {
+        title: course.title,
+        price: course.price,
+        githubRepo: course.githubRepo,
+      },
+    });
+  } catch (err) {
+    console.log("Buy Course Error:", err);
+
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
+};
+
+module.exports = { getAllCourses, getCourseBySlug, buyCourse };
---
+++ src/models/userCourses.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const userCoursesSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    course: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+userCoursesSchema.index({ user: 1, course: 1 }, { unique: true });
+
+module.exports = mongoose.model("UserCourses", userCoursesSchema);
