bb2e865f859f15d99aacccfefae79c2ad66d6d96
ShahnawazSaddam16 <saddammaqsood4321@gmail.com>
2026-07-23 19:34:59 +0500

    adding more courses in it

--- src/models/course.js
+++ src/models/course.js
@@ -19,7 +19,7 @@ const courseSchema = new mongoose.Schema(
     technology: {
       type: String,
       required: true,
-      enum: ["html", "css", "javascript", "react", "python"],
+      enum: ["html", "css", "javascript", "react", "python", "nodejs", "c", "sql", "mongodb"],
       trim: true,
     },
 
