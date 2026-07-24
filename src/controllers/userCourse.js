28ec87feb671f048e1d74898b9f47c28415bb8e5
ShahnawazSaddam16 <saddammaqsood4321@gmail.com>
2026-07-24 23:40:39 +0500

    making user controller btns

---
+++ src/controllers/userCourse.js
@@ -0,0 +1,15 @@
+const userCourses = require("../models/userCourses");
+
+const userCourse = async(req,res)=>{
+    try{
+        const courses = await userCourses.find({userId: req.user._id}).sort({createdAt: -1});
+
+        if(!courses){
+            return res.status(404).json({success: false, message: "Course Found on this Account"});
+        }
+
+        return res.status(200).json({success: true, courses});
+    } catch(err){
+        return res.status(500).json({success: false, message: err});
+    }
+}
