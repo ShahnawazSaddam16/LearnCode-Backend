960e4aa1ae67c76afd6c1b339a5e18495e414e27
Shahnawaz saddam Butt <saddammaqsood4321@gmail.com>
2026-07-28 19:41:45 +0500

    Update contact.js

--- src/models/contact.js
+++ src/models/contact.js
@@ -1,11 +1,6 @@
 const mongoose = require("mongoose");
 
 const contactSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-    },
     email: {
         type: String,
         lowercase: true,
